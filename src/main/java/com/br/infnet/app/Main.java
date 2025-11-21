7a1829682d9df0c285eb20301629818698e81b11
Larissa Conti <laricbarros@gmail.com>
2025-11-21 15:20:26 -0300

    adaptado main para que o javalin exponha porta externa

--- src/main/java/com/br/infnet/app/Main.java
+++ src/main/java/com/br/infnet/app/Main.java
@@ -8,10 +8,12 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
+        String host = System.getenv().getOrDefault("JAVALIN_HOST", "0.0.0.0");
+        int port = Integer.parseInt(System.getenv().getOrDefault("JAVALIN_PORT", "7000"));
         Javalin app = Javalin.create();
         iLivroRepository livroRepository = new LivroRepositoryImpl();
         new LivroController(app, livroRepository);
         new EmprestimoController(app, livroRepository);
-        app.start(7000);
+        app.start(host, port);
     }
 }
